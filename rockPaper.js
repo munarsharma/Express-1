03777c2e8325a46b4ba76181d79ecfdeb7fac1a7
Muna Sharma <munasharma@pursuit.org>
2018-12-03 19:04:46 -0500

    rockPaperDone

--- rockPaper.js
+++ rockPaper.js
@@ -1,23 +1,36 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-
-
-app.get('/:choice', (req, res) => {
+app.get("/:choice", (req, res) => {
   let aiChoice = Math.floor(Math.random() * 3);
-  let options = ['rock', 'paper', 'scissors'];
-   aiChoice = options[aiChoice];
-   let result =  [{req.params.choice:'rock', aiChoice:'rock', winninngChoice:'tie'}, {req.params.choice:'rock', aiChoice:'paper', winninngChoice: aiChoice} {req.params.choice:'rock', aiChoice:scissors, winninngChoice:req.params.choice}
-   {req.params.choice:'paper', aiChoice:'scissors', winninngChoice: aiChoice}
-   {req.params.choice:'scissors', aiChoice:'paper', winninngChoice:req.params.choice}
-   {req.params.choice:'scissors', aiChoice:'rock', winninngChoice: aiChoice} ]
 
-   const obj= {`user:${req.params.choice} , ai: ${aiChoice} , result: ${result}`}
-   res.json(obj);
+  let rock = [
+    { user: "rock", ai: "rock", result: "tie" },
+    { user: "rock", ai: "paper", result: "lose" },
+    { user: "rock", ai: "scissors", result: "win" }
+  ];
+  let paper = [
+    { user: "paper", ai: "paper", result: "tie" },
+    { user: "paper", ai: "scissors", result: "lose" },
+    { user: "paper", ai: "rock", result: "win" }
+  ];
+
+  let scissors = [
+    { user: "scissors", ai: "scissors", result: "tie" },
+    { user: "scissors", ai: "rock", result: "lose" },
+    { user: "scissors", ai: "paper", result: "win" }
+  ];
+
+  if (req.params.choice === "rock") {
+    res.json(rock[aiChoice]);
+  } else if (req.params.choice === "paper") {
+    res.json(paper[aiChoice]);
+  } else if (req.params.choice === "scissors") {
+    res.json(scissors[aiChoice]);
+  }
 });
 
-
 app.listen(8000, () => {
-  console.log('you are listening to port 8000');
+  console.log("you are listening to port 8000");
 });
